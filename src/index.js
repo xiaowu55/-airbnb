bd502b07ef16ce2da61ba25eab3b165cb61fbe8f
xiaowutonxue <jiaze@xiaowutxuedeMBP.lan>
2023-02-17 16:00:51 +0800

    modified:   README.md 	craco.config.js 	src/App.jsx 	src/assets/ 	src/router/ 	src/services/ 	src/store/index.js 	src/store/modules/entire/constants.js 	src/store/modules/entire/createActions.js 	src/store/modules/entire/index.js 	src/store/modules/home.js 	src/views/

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from '@/App';
+import './assets/css/index.less'
+import 'normalize.css'
+import store from './store'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
+    </Suspense>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
