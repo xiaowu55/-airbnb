9a33e4b9cf1200e83c740a5aedfd3240494ebccd
jiazewu <jiazewu001@gmail.com>
2023-02-25 19:10:35 +0800

    HomeSeectionV1

---
+++ src/views/home/c-cpns/home-section-v1/index.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+
+const HomeSectionV1 = memo((props) => {
+  return (
+    <div>HomeSectionV1</div>
+  )
+})
+
+HomeSectionV1.propTypes = {}
+
+export default HomeSectionV1
